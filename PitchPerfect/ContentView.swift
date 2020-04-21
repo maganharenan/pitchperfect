079170f8888aded36d9a22da1fc9b9caa3a43c87
Renan Maganha <maganha.renan@gmail.com>
2020-04-21 08:27:22 -0300

    Initial Commit

---
+++ PitchPerfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PitchPerfect
+//
+//  Created by Nuxen on 21/04/20.
+//  Copyright © 2020 Renan Maganha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
